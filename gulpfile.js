ea18bdca1409367fb038676f39777c081dd59718
Zakk <zmonsanu@gmail.com>
2017-12-27 16:02:25 +0000

    Deploy to Heroku 8

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,22 @@ var browserSync=require('browser-sync').create();
 var sass = require('gulp-sass');
 
 //compile sass into css & auto-inject into browser
+//Para HEROKU: debe ir en ruta:
+//  '/app/node_modules/bootstrap/scss/bootstrap.scss'
 gulp.task('sass',function(){
-  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss','src/scss/*.scss'])
+  return gulp.src(['/app/node_modules/bootstrap/scss/bootstrap.scss','src/scss/*.scss'])
           .pipe(sass())
-          .pipe(gulp.dest("src/css"))
+          .pipe(gulp.dest("app/src/css"))//para Heroku añadir app/src/scss
           .pipe(browserSync.stream());
 });
 
 
 //Move the JavaScript files into our /src/js folder
+//Para HEROKU: debe ir en ruta:
+//   ''/app/node_modules/bootstrap/dist/js/bootstrap.min.js'
 gulp.task('js',function(){
-  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js','node_modules/jquery/dist/jquery.min.js','node_modules/tether/dist/js/tether.min.js'])
-  .pipe(gulp.dest("src/js"))
+  return gulp.src(['/app/node_modules/bootstrap/dist/js/bootstrap.min.js','node_modules/jquery/dist/jquery.min.js','node_modules/tether/dist/js/tether.min.js'])
+  .pipe(gulp.dest("app/src/js")) //para Heroku añadir app/
   .pipe(browserSync.stream());
 });
 
@@ -24,8 +28,8 @@ gulp.task('serve',['sass'],function(){
 browserSync.init({
   server:"./src"
 });
-  gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss','src/scss/*.scss'],['sass']);
-  gulp.watch("src/*.html").on('change',browserSync.reload);
+  gulp.watch(['/app/node_modules/bootstrap/scss/bootstrap.scss','src/scss/*.scss'],['sass']);
+  gulp.watch("app/src/*.html").on('change',browserSync.reload); //para Heroku añadir app/
 });
 
 
